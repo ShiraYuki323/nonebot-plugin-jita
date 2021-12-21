9db6240070325d39fc3d9acc051165453363a9c4
ShiraYuki323 <76277519+ShiraYuki323@users.noreply.github.com>
2021-12-21 17:39:51 +0800

    Add files via upload
    优化

--- nonebot-plugin-jita/__init__.py
+++ nonebot-plugin-jita/__init__.py
@@ -1,6 +1,7 @@
 from ssl import OP_NO_COMPRESSION
+from nonebot.adapters.cqhttp import message
 import xlrd
-from .getdata import getjita,pregetjita
+from .getdata import getjita,pregetjita,colgetjita
 from nonebot import on_command
 from nonebot.adapters.cqhttp import Bot,Event, Message, bot
 from nonebot.adapters.cqhttp.message import MessageSegment
@@ -9,7 +10,7 @@ from nonebot.typing import T_State
 
 ojita = on_command('ojita',priority=5)
 opjita = on_command('opjita',priority=5)
-
+col = on_command('col',priority=5)
 
 @ojita.handle()
 async def _(bot : Bot ,event: Event,state: T_State):
@@ -35,8 +36,20 @@ async def _(bot : Bot ,event: Event,state: T_State):
 async def hanlde_id(bot: Bot ,event: Event,state: T_State):
     gotid = state["id"]
     getval = await pregetjita(gotid)
-    await ojita.finish(getval)    
+    await opjita.finish(getval)    
 
+@col.handle()
+async def _(bot : Bot ,event: Event,state: T_State):
+    args = str(event.get_message()).strip()    
+    if args:        
+        state["id"] = args
+
+@col.got("id",prompt="想查询什么物品呢？")
+async def hanlde_id(bot: Bot ,event: Event,state: T_State):
+    gotid = state["id"]
+    getval = await colgetjita(gotid)
+    Message(getval)
+    await col.finish(getval)   
 
 
 
