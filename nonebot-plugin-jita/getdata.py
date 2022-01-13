16406674ee8a923c1c0cd25a7ec64397ce57e369
ShiraYuki323 <76277519+ShiraYuki323@users.noreply.github.com>
2022-01-13 21:11:28 +0800

    更改默认数据源获取地址
    由于evemarket的api炸了 改用国产api

--- nonebot-plugin-jita/getdata.py
+++ nonebot-plugin-jita/getdata.py
@@ -1,51 +1,55 @@
 import xmltodict
-import ssl
-import aiohttp
+from aiohttp import ClientSession
+from aiohttp import TCPConnector
 from .evedata import eved
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"}
-ssl._create_default_https_context = ssl._create_unverified_context
+
 
 eve_types = eved.eve_types
 listLen = len(eve_types)
 async def getmkt(id)->dict:
-    try:       
-        url = (f"https://api.evemarketer.com/ec/marketstat?usesystem=30000142&typeid={id}")
-        async with aiohttp.ClientSession() as Session:
-            async with Session.get(url=url,headers=headers) as rep:
-                context = await rep.read()
-        dexml = xmltodict.parse(context)
-        jitabuy = float(dexml['exec_api']['marketstat']['type']['buy']['max'])
-        jitasell = float(dexml['exec_api']['marketstat']['type']['sell']['min'])
-        jita = {'buy':jitabuy,'sell':jitasell}
-        return jita
-    except:
+    try:
         url = (f"https://www.ceve-market.org/tqapi/marketstat?typeid={id}&usesystem=30000142")
-        async with aiohttp.ClientSession() as Session:
+        async with ClientSession(connector=TCPConnector(verify_ssl=False)) as Session:
             async with Session.get(url=url,headers=headers) as rep:
                 context = await rep.read()
         dexml = xmltodict.parse(context)
         jitabuy = float(dexml['evec_api']['marketstat']['type']['buy']['max'])
         jitasell = float(dexml['evec_api']['marketstat']['type']['sell']['min'])
         jita = {'buy':jitabuy,'sell':jitasell}
-        return jita
+        return jita           
+    except Exception as e:
+        print(e)
+        url = (f"https://api.evemarketer.com/ec/marketstat?usesystem=30000142&typeid={id}")
+        async with ClientSession(connector=TCPConnector(verify_ssl=False)) as Session:
+            async with Session.get(url=url,headers=headers) as rep:
+                context = await rep.read()
+        dexml = xmltodict.parse(context)
+        jitabuy = float(dexml['exec_api']['marketstat']['type']['buy']['max'])
+        jitasell = float(dexml['exec_api']['marketstat']['type']['sell']['min'])
+        jita = {'buy':jitabuy,'sell':jitasell}
+        return jita   
 
 async def GetItem(CnItem):
-    num = 0
-    for i in eve_types:
-        if CnItem in eve_types[i][0]:
-            ItemId = i
-            ItemName = eve_types[i][0]
-            ItemDir={'id':ItemId,'name':ItemName}
-            print(ItemDir)
-            return ItemDir
-        else:
-            num = num+1
-            if num == listLen:
-                return "表内无此物品"
+    if CnItem in ('PLEX','plex','伊甸币'):
+        return {'id':'44992','name':'伊甸币'}
+    else:
+        num = 0
+        for i in eve_types:
+            if CnItem in eve_types[i][0]:
+                ItemId = i
+                ItemName = eve_types[i][0]
+                ItemDir={'id':ItemId,'name':ItemName}
+                #print(ItemDir)
+                return ItemDir
+            else:
+                num = num+1
+                if num == listLen:
+                    return "表内无此物品"
 
 async def PGetItem(CnItem):
     num = 0
-    for i in range(eve_types):
+    for i in eve_types:
         if CnItem == eve_types[i][0]:
             ItemId = i
             ItemName = eve_types[i][0]
@@ -79,9 +83,15 @@ async def getjita(GetCnItem):
             jitamkt = await getmkt(item['id'])
             if(jitamkt['buy'] == 0.00 and jitamkt['sell'] == 0.00 ):
                 return (f"{item['name']}\n该物品买卖价格均为0")
-            else:          
+            else:
                 jitabuy = format(jitamkt['buy'],',')     
-                jitasell = format(jitamkt['sell'],',')        
+                jitasell = format(jitamkt['sell'],',') 
+                if item['id'] == '44992':
+                    plexjitabuy = format((jitamkt['buy']*500),',')     
+                    plexjitasell = format((jitamkt['sell']*500),',') 
+                    return (f"{item['name']}\njitabuy:\n{jitabuy} ISK\njitasell:\n{jitasell} ISK\n{item['name']}*500\njitabuy:\n{plexjitabuy} ISK\njitasell:\n{plexjitasell} ISK")   
+                else:
+                    pass                         
                 return (f"{item['name']}\njitabuy:\n{jitabuy} ISK\njitasell:\n{jitasell} ISK")
         else:
             return "无法查询此物品,表内可能没有此物品"
